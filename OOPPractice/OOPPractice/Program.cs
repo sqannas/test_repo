86791b3e3d8bffd19e2ffd94a7d08f0be7143386
SQannas <saed.alqannas@live.com>
2017-01-19 22:41:41 -0500

    New Project

---
+++ OOPPractice/OOPPractice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Rectangle rect = new Rectangle(4, 6);
+            Console.WriteLine("Height: {0}", rect.Height);
+            Console.WriteLine("Width: {0}", rect.Width);
+
+            Rectangle square = new Rectangle(5);
+            Console.WriteLine("Height: {0}", square.Height);
+            Console.WriteLine("Width: {0}", square.Width);
+
+        }
+    }
+}
---
+++ OOPPractice/OOPPractice/Rectangle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPractice
+{
+    class Rectangle
+    {
+        public int Height { get; }
+        public int Width { get; }
+
+        public Rectangle(int height, int width)
+        {
+            if (height <= 0) throw new ArgumentException("height");
+            if (width <= 0) throw new ArgumentException("width");
+
+            Height = height;
+            Width = width;
+
+            Console.WriteLine("Rectangle Constructor Called");
+        }
+
+        //public Rectangle(int size)
+        //{
+        //    if (size <= 0) throw new ArgumentException("height");
+
+        //    Height = Width = size;
+
+        //    Console.WriteLine("Square Constructor Called");
+        //}
+
+
+        public Rectangle(int size) : this(size, size)
+        {
+            Console.WriteLine("Square Constructor Called");
+        }
+    }
+}
